970ba7ca6c7739bbdd641e075f7774f7ea907f83
Yusuf Kathrada <kthyus001@myuct.ac.za>
2024-05-06 11:40:06 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,20 @@
+import socket
+
+#Create socket object
+clientsocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+
+#host = "0.0.0.0"
+host = socket.gethostname()
+
+port = 1200
+
+clientsocket.connect((host, port)) #You can substitue the host with the server IP
+
+#Receiving a maximum of 1024 bytes
+message = clientsocket.recv(1024)
+
+clientsocket.close()
+
+print(message.decode('utf-8'))
+# Test
+#Please
---
+++ server.py
@@ -0,0 +1,28 @@
+import socket
+
+#Creating the socket object
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+host = "0.0.0.0"
+port = 1200 
+
+#Binding to socket
+serversocket.bind((host, port))
+
+#Starting TCP listener
+serversocket.listen(3)
+
+while True:
+    #Starting the connection 
+    clientsocket,address = serversocket.accept()
+
+    print("received connection from " + str(address))
+    
+    #Message sent to client after successful connection
+    message = 'Hello! Thank you for connecting to the server' + "\r\n"
+    
+    clientsocket.send(message.encode('utf-8'))
+
+    clientsocket.close()
+# Test
+#please
