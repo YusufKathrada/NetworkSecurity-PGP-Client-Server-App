e72c4850444b0e240e733a8d6f7d02783c4a0063
Yusuf Kathrada <kthyus001@myuct.ac.za>
2024-05-07 20:22:23 +0200

    feat: Add PGP encryption and decryption scripts
    The code changes include the addition of PGP encryption and decryption scripts. These scripts allow for the encryption and decryption of files using Alice's public and private keys. The scripts utilize the gnupg library and provide functionality for importing keys, setting trust levels, encrypting files, decrypting files, and verifying signatures.
    
    The commit message suggests adding PGP encryption and decryption scripts to the repository.

---
+++ pgp_genkey.py
@@ -0,0 +1,38 @@
+#This file creates a PGP key pair using the gnupg library
+
+import os
+import gnupg
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+gpg_home = os.path.join(script_dir, '.gnupg')
+gpg = gnupg.GPG(gnupghome = gpg_home, gpgbinary='C:\\Program Files (x86)\\GnuPG\\bin\\gpg.exe')
+
+#created keys will be stored in this folder
+#gpg = gnupg.GPG(gnupghome='/.gnupg')
+
+gpg.encoding = 'utf-8'
+
+#inputs to generate the keys
+input_data = gpg.gen_key_input(
+    name_email = 'alice@example.com',
+    key_type="RSA",
+    key_length=1024,
+    passphrase='passphrase',
+)
+
+#generating the key pairs (public and private)
+key = gpg.gen_key(input_data)
+
+#catch case where key did not generate
+if not key:
+    print("Key generation failed.")
+else:
+    print("Key generation result:", key)
+
+#export the public key
+public_key = gpg.export_keys(key.fingerprint)
+
+with open('alice_public_key.asc', 'w') as f:
+    f.write(public_key)
+
+#TODO: send this public key to Bob/ other clients
